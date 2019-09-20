bff366c32f7e8670b6e74d88fe9eba36835c6d14
Viktoriia <viktoria.nosik@gmail.com>
2019-09-20 09:40:10 +0200

    add getRandomElement function

--- js/setup.js
+++ js/setup.js
@@ -8,29 +8,18 @@ var WIZARDS_NAME = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARDS_SURNAME = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
 var WIZARDS_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARDS_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomElement = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
 var wizards = [];
-
-var getName = function () {
-  return WIZARDS_NAME[Math.floor(Math.random() * WIZARDS_NAME.length)];
-};
-
-var getSurname = function () {
-  return WIZARDS_SURNAME[Math.floor(Math.random() * WIZARDS_SURNAME.length)];
-};
-
-var getCoatColor = function () {
-  return WIZARDS_COAT_COLOR[Math.floor(Math.random() * WIZARDS_COAT_COLOR.length)];
-};
-
-var getEyesColor = function () {
-  return WIZARDS_EYES_COLOR[Math.floor(Math.random() * WIZARDS_EYES_COLOR.length)];
-};
 
 for (var i = 0; i < 4; i++) {
   wizards.push({
-    name: getName() + getSurname(),
-    coatColor: getCoatColor(),
-    eyesColor: getEyesColor()
+    name: getRandomElement(WIZARDS_NAME) + getRandomElement(WIZARDS_SURNAME),
+    coatColor: getRandomElement(WIZARDS_COAT_COLOR),
+    eyesColor: getRandomElement(WIZARDS_EYES_COLOR)
   });
 }
 
